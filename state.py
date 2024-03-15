43c3c32e98b653538652b8f3e21d0275870f624f
ryddk <ryddk0301@gmail.com>
2024-03-16 03:40:20 +0900

    save

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pyecharts.charts import Bar
+from pyecharts.charts import Bar,Pie
 from pyecharts import options as opts
 from streamlit_echarts import st_pyecharts
 
@@ -17,20 +17,36 @@ def get_new_df():
     return new_df, new_columns
 
 def lib_status(new_df, new_columns):
-    
-    # 막대그래프로 그리기
+
+    ## 막대그래프로 그리기
+    # 리스트로 변경
     x = new_columns
     y = new_df.iloc[1].tolist()
 
-    bar = (
+    bar_chart = (
         Bar()
         .add_xaxis(x)
-        .add_yaxis("갯수(권)",y)
+        .add_yaxis("분류",y, label_opts=False)
         .set_global_opts(title_opts=opts.TitleOpts(title = "서울도서관 분야별 장서보유 현황",
-                                                    subtitle= "마우스를 막대 위에 올려 정확한 수치를 확인해보세요!"))       
+                                                    subtitle= "마우스를 막대 위에 올리면 정확한 수치 확인이 가능합니다"))       
     )
 
-    st_pyecharts(bar)
+
+    ## 파이차트로 그리기
+    # 파이 그래프에 사용될 데이터셋
+    z = [list(i) for i in zip(x,y)]
+
+    pie_chart = (
+        Pie()
+        .add("",z, radius=["30%","80%"]) # radius : 내부 및 외부 링 크기
+        .set_global_opts(title_opts=opts.TitleOpts(title="파이그래프",subtitle="마우스를 원 위에 올리면 정확한 수치 확인이 가능합니다"),
+                         legend_opts=opts.LegendOpts(orient="vertical", pos_top="20%", pos_left="5%")) # 범례 조정
+        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b} : ({d}%)"))
+    )
+
+
+    st_pyecharts(bar_chart)
+    st_pyecharts(pie_chart)
 
 def lib_status_result():
     data, columns_name = get_new_df()
