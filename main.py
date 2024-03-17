e07617564f499d27bd8a0c89e0083a6b7e230ffa
ryddk <ryddk0301@gmail.com>
2024-03-18 04:51:00 +0900

    test

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import matplotlib.pyplot as plt
 from loan_total import loan_total_page
 from state import lib_status_result
 from test import prefer
 
 
-def main():
-    st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
-    with st.sidebar:
-        choose = option_menu("Menu",
-            options= ["장서현황", "대출누적", "연령대별 대출누적", "인기대출도서"],
-            icons = ["bi bi-list","bi bi-list","bi bi-list","bi bi-list"],
-            default_index = 0
-        )
-    # st.title("서울도서관")
-
-    if choose == "장서현황":
-        lib_status_result()
-
-    if choose == "대출누적":
-        loan_total_page()
-
-    if choose == "연령대별 대출누적":
-        pass
-
-    if choose == "인기대출도서":
-        prefer()
-        
+# 옵션에 따라 실행할 함수들을 매핑한 딕셔너리 생성
+option_functions = {
+    "MAIN" : lib_status_result,
+    "장서현황": lib_status_result,
+    "대출통계": prefer,
+    "연령대별 대출통계": prefer,
+    "아직못정함" : lib_status_result
+}
 
 
-if __name__ == "__main__":
-    main()
+# 옵션 메뉴 생성
+with st.sidebar:
+    selected_option = option_menu("MENU", ["MAIN","장서현황", "대출통계", "연령대별 대출통계", '아직못정함'], 
+                                icons=['house', 'cloud-upload', "list-task", 'gear'],
+                                default_index=0
+                                #  with st.sidebar 지우고 orientation="horizontal" 이거하면 
+    )
+
+
+# 선택된 옵션에 맞는 함수 실행
+if selected_option in option_functions:
+    option_functions[selected_option]()
+
+
